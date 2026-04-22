12ebe0c9378e924be2f215c5265c77b1a61b72a4
Tomas <tomas@local>
2026-04-22 15:37:25 -0400

    fix: refresh cached app and expose latest changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'boss-mode-v1';
+const CACHE = 'boss-mode-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -9,9 +9,15 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
